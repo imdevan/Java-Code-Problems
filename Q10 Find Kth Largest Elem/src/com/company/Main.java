a9d60ea482ed34ba74866713b48844d607a072c7
huapayadevan <huapayadevan@gmail.com>
2014-12-14 11:40:53 -0600

    Added Q11 Find All Permuations of a String

--- Q10 Find Kth Largest Elem/src/com/company/Main.java
+++ Q10 Find Kth Largest Elem/src/com/company/Main.java
@@ -5,13 +5,15 @@ import java.util.Random;
 
 public class Main {
 
+    // Cheat-y implementation
+    // ------------------------------------------------------------------
     public static int cheatyFindK(int[] a, int k)
     {
         Arrays.sort(a);
         return a[k-1];
     }
 
-    // proper implementation
+    // Proper implementation
     // ------------------------------------------------------------------
     // Helper function to swap variables
     public static void swap(int[] a, int upper, int lower)
@@ -76,17 +78,70 @@ public class Main {
         return halfSort(a, 0, a.length-1, k);
     }
 
+
+    // Median of medians implementation
+    // ------------------------------------------------------------------
+    public static int partition2(int[] a, int s, int e, int p)
+    {
+        int swi = s;
+        for(int i = s; i < e; i ++)
+            if(a[i] <= p)
+            {
+                swap(a, swi, i);
+                swi++;
+            }
+
+        swap(a, swi, e);
+        return swi;
+    }
+    public static int findKLargestWithMedianOfMedians(int [] a, int s, int e, int k)
+    {
+        int length = e-s+1;
+        if(k<=1|| k >=length)
+            return -1;
+        else if (length <= 5)
+            return a[s+ k-1];
+
+        int numOfMedians = length/5;
+
+        int [] medians = new int[numOfMedians];
+
+        for(int i = 0; i < numOfMedians; i ++)
+            medians[i] = findKLargestWithMedianOfMedians(a, s+5*1, s+(i+1)*5-1, k);
+
+        int pivot = findKLargestWithMedianOfMedians(medians, 0, medians.length-1, medians.length/2 + 1);
+
+        int pivotIndex = partition2(a, s, e, k);
+        int rank = pivotIndex +1;
+
+        if(rank == k)
+            return a[pivotIndex];
+        else if(rank >= k)
+            return findKLargestWithMedianOfMedians(a, 0, pivotIndex, k);
+        else
+            return findKLargestWithMedianOfMedians(a, pivotIndex+1, e, k-rank);
+    }
+
+    public static int findKLargestMofM(int [] a, int k)
+    {
+        return findKLargestWithMedianOfMedians(a, 0, a.length-1, k);
+    }
+
     public static void main(String[] args)
     {
         // Test array
-        int[] testA = {3,1,2,1,4};
+        int[] testA = {3,1,2,1,4,3,5135,32,2,5,5,4,2,12,2,5,3,54,34,7,1,34};
         int k = 3;
 
         System.out.println("Proper Method");
         System.out.println(findKlargest(testA, k));
 
+
         System.out.println("Cheat-y Method");
         System.out.println(cheatyFindK(testA, k));
+
+        System.out.println("Median Of Medians Method");
+        System.out.println(findKLargestMofM(testA, k));
 
 
     }
---
+++ Q11 All Permutations of a String/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<String> findAllPermuations(String s)
+    {
+        // Result ArrayList
+        ArrayList<String> r = new ArrayList<String>();
+
+        // Termination case
+        if(s.length() <= 1)
+        {
+            r.add(s);
+            return r;
+        }
+
+        // Remove one character from string
+        char c = s.charAt(0);
+
+        // Find all permuations of length N-1
+        ArrayList<String> permList = findAllPermuations(s.substring(1));
+
+        // Iterate over permuations
+        for(String perm : permList)
+            // Insert char into each place of permuation
+            for(int i = 0; i <= perm.length(); i++)
+                r.add(
+                        perm.substring(0,i) + c + perm.substring(i)
+                );
+
+        // Return result
+        return r;
+    }
+
+    public static void main(String[] args) {
+        // Test String
+        String test = "hello";
+
+        // Result
+        System.out.print(findAllPermuations(test).toString());
+    }
+}
